3ff1b668f8be656d62023bc1fb91a6e287fb82a4
EvilJagaGenius <EvilJagaGenius@users.noreply.github.com>
2021-02-01 09:38:37 -0500

    Added routes + dummy pages

--- server.py
+++ server.py
@@ -12,6 +12,30 @@ def cleanup(exception):
     # Do something, Taipu
     pass
 
+@app.route("/login/")
+def loginGET():
+    return render_template("login.html")
+
+@app.route("/profile/")
+def profileGET():
+    return render_template("profile.html")
+
+@app.route("/join/")
+def joinGET():
+    return render_template("join.html")
+
+@app.route("/play/")
+def playGET():
+    return render_template("play.html")
+
+@app.route("/mytemplates/")
+def myTemplatesGET():
+    return render_template("mytemplates.html")
+
+@app.route("/postgame/")
+def postGameGET():
+    return render_template("postgame.html")
+
 # /home/ (default route)
 @app.route("/home/")
 @app.route("/")

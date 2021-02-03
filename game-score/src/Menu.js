24c6d4c204d44768981213cfcf26220ac4a08d19
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2021-02-02 19:41:52 -0500

    Added React and Accordian menus
    Created the necessary files for React and implemented the beginning for the accordian menus for the home screen.

---
+++ game-score/src/Menu.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Accordion, Icon } from 'semantic-ui-react'
+
+export default class Menu extends Component {
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render() {
+    const { activeIndex } = this.state
+
+    return (
+      <Accordion fluid styled>
+        <Accordion.Title
+          active={activeIndex === 0}
+          index={0}
+          onClick={this.handleClick}
+        >
+          <Icon name='dropdown' />
+          Favorited Templates
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          <p>
+            Data go here
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 1}
+          index={1}
+          onClick={this.handleClick}
+        >
+          <Icon name='dropdown' />
+          Recently Played
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          <p>
+            Data go here
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 2}
+          index={2}
+          onClick={this.handleClick}
+        >
+          <Icon name='dropdown' />
+          Highest Rated Templates
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <p>
+            Data go here
+          </p>
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 3}
+          index={3}
+          onClick={this.handleClick}
+        >
+          <Icon name='dropdown' />
+          Recommended Games
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 3}>
+          <p>
+            Data go here
+          </p>
+        </Accordion.Content>
+      </Accordion>
+    )
+  }
+}
---
+++ game-score/src/index.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import Menu from "./Menu";
+
+// TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Menu />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

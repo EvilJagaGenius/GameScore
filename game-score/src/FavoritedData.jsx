c13e4ea8a37246807c4bc67c45124d47fa411846
buckeye144 <44789411+buckeye144@users.noreply.github.com>
2021-02-09 20:19:34 -0500

    test for JSON reading
    Attempting a test for fixing the JSON read issue. Also created a file for the TemplateEditor

--- game-score/src/FavoritedData.jsx
+++ game-score/src/FavoritedData.jsx
@@ -7,20 +7,22 @@ export default function FavoritedData() {
   //request data from flask server and assign data accordingly
   useEffect(() => {
     fetch('/api/getHomePage').then(res => res.json()).then(data => {
-      setTemplateData(data.favoritedTemplates);
+      setTemplateData(data.favoritedTemplates[0]);
     });
   }, []);
 
   return(
-    <table>
-      {templateData.map(template => (
-        <tr>
-            <td>{template.pictureURL}</td>
+    <div className="FavoritedData">
+      <table>
+        {templateData.map((template) => (
+          <tr>
+            <td><img src={template.pictureURL}/></td>
             <td>{template.templateName}</td>
             <td>{template.NumRatings}</td>
             <td>{template.averageRating}</td>
-        </tr>
-      ))}
-    </table>  
+          </tr>
+        ))}
+      </table>  
+    </div>
   );
 }

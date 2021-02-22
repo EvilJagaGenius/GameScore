e9d1690abb21dfb6c8d7f119bc09a827832ac53f
Jonathon Lannon <jonathonlannon@icloud.com>
2021-02-22 04:10:25 -0500

    Field checking phase 1

--- game-score/src/pages/loginPages/ForgetPassword.jsx
+++ game-score/src/pages/loginPages/ForgetPassword.jsx
@@ -6,28 +6,76 @@ import React from "react";  //basic React framework
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from '@material-ui/core/styles';
 import {Button} from "@material-ui/core";  //Material UI for tab bar
+import Box from '@material-ui/core/Box';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-//import Logo from '../images/GameScore App Logo.png';
+import { Component } from "react";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-  }));
-
-  export default function ForgetPassword() {
-    const classes = useStyles();
-    return (
-      <form className={classes.root} noValidate autoComplete="off">
-        <h1>Forget Password</h1>
-        <div>
-          <TextField required id="standard-required" label="Username"/>
-          <TextField required id="standard-required" label="Password" type="password"/>
-        </div>
-        <Link to="/home"><Button>Click here</Button></Link>
-      </form>
-    );
+export default class ForgetPassword extends Component{
+  constructor(props){
+    super();
+    this.state = {
+      username: "",
+      email: ""
+    }
   }
+
+  usernameHandler=(event)=>{
+    this.setState({
+      username: event.target.value
+    });
+    console.log(this.state.username);
+  }
+
+  emailHandler=(event)=>{
+    let email = event.target.value;
+    if(email.includes("@") && email.includes(".")){
+      this.setState({
+        email: event.target.value
+      })
+    }
+    else{
+      this.setState({
+        email: ""
+      })
+    }
+  }
+
+  /**
+   * confirmSubmission: function for handling submission events
+   */
+  confirmSubmission(){
+    alert(this.state.email);
+  }
+
+  render(){
+  const classes = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+return (
+  <form className={classes.root} noValidate autoComplete="off">
+    <h1>Login Page</h1>
+    <Box m={2} pt={3}>
+    <div>
+      <TextField required id="standard-required" label="Username" onChange={this.usernameHandler}/>
+    </div>
+    <div>
+      <Link to="/home/login"><Button onClick={()=>{alert(this.state.username)}}>Reset</Button></Link>
+    </div>
+    <div>
+      <TextField required id="standard-required" label="Email Address" onChange={this.emailHandler}/>
+    </div>
+    <div>
+      <Link to="/home/login"><Button onClick={()=>{
+        this.confirmSubmission()
+        }}>Reset</Button></Link>
+    </div>
+    </Box>
+  </form>
+  );
+  }
+}
--- game-score/src/pages/loginPages/Login.jsx
+++ game-score/src/pages/loginPages/Login.jsx
@@ -8,29 +8,45 @@ import {makeStyles} from '@material-ui/core/styles';
 import {Button} from "@material-ui/core";  //Material UI for tab bar
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Logo from '../../images/GameScore App Logo.png';
+import { Component } from "react";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-  }));
-
-  export default function Login() {
-    const classes = useStyles();
-    return (
-      <form className={classes.root} noValidate autoComplete="off">
-        <img src={Logo} alt="GameScore Logo" width="100" height="100"></img>
-        <h1>Login Page</h1>
-        <div>
-          <TextField required id="standard-required" label="Username"/>
-          <TextField required id="standard-required" label="Password" type="password"/>
-        </div>
-        <Link to="/home"><Button>Click here</Button></Link>
-        <Link to="/login/forgetpassword"><Button>Forget Password?</Button></Link>
-        <Link to="/login/createaccount"><Button>Create Account</Button></Link>
-      </form>
-    );
+export default class Login extends Component{
+  constructor(props){
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    }
   }
+  usernameHandler=(event)=>{
+    this.setState({
+      username: event.target.value
+    });
+    console.log(this.state.username);
+  }
+  render(){
+  const classes = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+return (
+  <form className={classes.root} noValidate autoComplete="off">
+    <img src={Logo} alt="GameScore Logo" width="100" height="100"></img>
+    <h1>Login Page</h1>
+    <div>
+      <TextField required id="standard-required" label="Username" onChange={this.usernameHandler} value = {this.state.username}/>
+    </div>
+    <div>
+      <TextField required id="standard-required" label="Password" type="password"/>
+    </div>
+    <Link to="/home"><Button onClick={()=>{alert(this.state.username)}}>Login</Button></Link>
+    <Link to="/login/forgetpassword"><Button>Forget Password?</Button></Link>
+    <Link to="/login/createaccount"><Button>Create Account</Button></Link>
+  </form>
+  );
+  }
+}

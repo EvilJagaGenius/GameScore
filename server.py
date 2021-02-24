e002e93982032a6df2b0082bccb20ff174e2f760
Benjamin <BENJAMINJA17@GCC.EDU>
2021-02-24 12:38:59 -0500

    Chanign CORS policy v2

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ def getUserID():
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Origin', response.setHeader("Access-Control-Allow-Origin", request.headers.origin);)
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')

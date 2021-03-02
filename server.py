1024ad41e55f09a64e135fcacb9bb9c248254166
JABC44 <62125077+JABC44@users.noreply.github.com>
2021-03-01 20:33:53 -0500

    Update server.py

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ socketIo = SocketIO(app, cors_allowed_origins="*")
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Origin', request.environ.get('HTTP_ORIGIN', 'default value'))
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')

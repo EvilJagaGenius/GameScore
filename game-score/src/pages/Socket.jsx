5c1f592ae91b029e4965ad64e5cd0c957673c74a
JABC44 <62125077+JABC44@users.noreply.github.com>
2021-03-03 09:31:20 -0500

    Update Socket.jsx

--- game-score/src/pages/Socket.jsx
+++ game-score/src/pages/Socket.jsx
@@ -5,7 +5,7 @@ export default class MySocket
 {
 	constructor()
 	{
-		this.personalSocket = io("localhost:5000");
+		this.personalSocket = io("gamescore.gcc.edu:5000");
 		this.personalSocket.emit("join",Cookies.get('credHash'),Cookies.get('username'))
 		console.log("SOcket Started")
 	}

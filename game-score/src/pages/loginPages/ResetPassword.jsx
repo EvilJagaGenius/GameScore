03ff7dd2c55248e0b68e7d02d135c1a77a59f7d0
Jonathon Lannon <jonathonlannon@icloud.com>
2021-03-23 03:13:59 -0400

    Reset password commnet additions and bug fixes

--- game-score/src/pages/loginPages/ResetPassword.jsx
+++ game-score/src/pages/loginPages/ResetPassword.jsx
@@ -1,8 +1,10 @@
 /**
  * ResetPassword.jsx-Jonathon Lannon
+ * React page for handling account password reset functionality
  */
 
-import React from "react";  //basic React framework
+//import resources
+import React from "react";
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from '@material-ui/core/styles';
 import {Button} from "@material-ui/core";
@@ -10,37 +12,66 @@ import Box from '@material-ui/core/Box';
 import { Component } from "react";
 import Logo from '../../images/GameScore App Logo.png';
 
+/**
+ * ResetPassword class: React component for resetting the user's password
+ * state @param
+ * password: string for holding the value in the password textfield
+ * confirmPassword: string for holding the value in the password textfield
+ * passwordError: boolean value deciding whether or not the textfield error property is on, if there is an error in the password textfield
+ * confirmPasswordError: boolean value deciding whether or not the textfield error property is on, if there is an error in the confirm password textfield
+ * token: the unique token code for the user to access this page and update their account
+ * data: variable for storing the JSON data recieved from the server
+ */
 export default class ResetPassword extends Component{
   constructor(props){
     super();
     this.state = {
       password: "",
+      confirmPassword: "",
       passwordError: false,
+      confrimPasswordError: false,
       token: "",
       data: ""
     }
   }
 
+  /**
+   * componentDidMount: React component state function for updating the component
+   */
   componentDidMount(){
+    //when the page is loaded, parse the url for the token, and store it in the token state variable
     this.setState({
       token: this.props.location.search.substr(this.props.location.search.indexOf("=")+1)
     });
   }
 
   /**
-   * passwordHandler
-   * @param {*} event 
+   * passwordHandler: function for handling password related error checking and events for the password textfield
+   * @param {*} event: event parameter for processing the new value in the password textfield
    */
   passwordHandler=(event)=>{
     console.log(event.target.value)
-    var pass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{4,30}/
-    if(String(event.target.value).match(pass)){
+    //string for handling the password requirements
+
+    /* Requirements are:
+        at least one number
+        at least one lowercase letter
+        at least one uppercase letter
+        4-30 characters in length
+    */
+    var passRequirements = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{4,30}/;
+
+    //if the string matches the requirements, make sure the error property is set to false for the textfield
+    //update the state for the password value
+    if(String(event.target.value).match(passRequirements)){
       this.setState({
         passwordError: false,
         password: event.target.value
       });
       console.log("requirements met");
     }
+    //else, enable the error property for the password textfield
+    //update the state for the password value
     else{
       console.log("requirments not met")
       this.setState({
@@ -51,18 +82,21 @@ export default class ResetPassword extends Component{
   }
 
   /**
-   * confirmPasswordHandler
-   * @param {*} event 
+   * confirmPasswordHandler: function for handling password related error checking and events for the password textfield
+   * @param {*} event: event parameter for processing the new value in the password textfield
    */
   confirmPasswordHandler=(event)=>{
+    //update the state for confirmPassword
     this.setState({
       confirmPassword: event.target.value
     });
+    //if both passwords don't match, display an error by making confirmPasswordError true
     if(String(event.target.value) !== String(this.state.password)){
       this.setState({
         confrimPasswordError: true
       });
     }
+    //otherwise, take away the error display
     else{
       this.setState({
         confrimPasswordError: false
@@ -70,8 +104,12 @@ export default class ResetPassword extends Component{
     }
   }
 
+  /**
+   * sendRequest: function for sending the password reset request to the server
+   */
   async sendRequest() {
-    // POST request using fetch with async/await
+    //POST request using fetch with async/await
+    //create the request with the needed options and parameters
     const requestOptions = {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -81,10 +119,11 @@ export default class ResetPassword extends Component{
         })
     };
     const response = await fetch('api/postResetPassword', requestOptions);
-    const data = await response.json();
-    this.setState({data: data.successful});
+    const data = await response.json();     //wait for the response from the server
+    this.setState({data: data.successful});   //update the data state so it can be accessed
     //errors and error message
     console.log(this.state.data);
+    //display an alert on whether or not the reset worked
     if(this.state.data === true){
       alert("Password reset successful");
     }
@@ -94,9 +133,10 @@ export default class ResetPassword extends Component{
   }
 
   /**
-   * confirmSubmission: function for handling submission events
+   * confirmSubmission: function for handling submission events when the Reset Password button is clicked
    */
   confirmSubmission(){
+    //check if no password is entered, if not alert the user
     if(this.state.password === ""){
       alert("No password entered");
       this.setState({
@@ -109,31 +149,36 @@ export default class ResetPassword extends Component{
     }
   }
 
+  /**
+   * render: React function for rendering the component
+   * @returns elements that will make up the on-screen component
+   */
   render(){
-  const classes = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
-return (
-  <form className={classes.root} noValidate autoComplete="off">
-    <Box m={2} pt={3}>
-    <img src={Logo} alt="GameScore Logo" width="100" height="100"></img>
-    <h1>Reset Password</h1>
-    <div>
-      <TextField required id="standard-required" name = "password" label="Password" type="password" onChange={this.passwordHandler}  error={this.state.passwordError}/>
-    </div>
-    <div>
-      <TextField required id="standard-required" name = "confirmpassword" label="Confirm Password" type="password" onChange={this.confirmPasswordHandler} error={this.state.passwordError}/>
-    </div>
-    <div>
-      <Button onClick={()=>{this.confirmSubmission()}}>Reset Password</Button>
-    </div>
-    </Box>
-  </form>
-  );
+  //material ui styling const
+    const classes = makeStyles((theme) => ({
+      root: {
+        '& .MuiTextField-root': {
+          margin: theme.spacing(1),
+          width: '25ch',
+        },
+      },
+    }));
+    return (
+      <form className={classes.root} noValidate autoComplete="off">
+        <Box m={2} pt={3}>
+        <img src={Logo} alt="GameScore Logo" width="100" height="100"></img>
+        <h1>Reset Password</h1>
+        <div>
+          <TextField required id="standard-required" name = "password" label="Password" type="password" onChange={this.passwordHandler}  error={this.state.passwordError}/>
+        </div>
+        <div>
+          <TextField required id="standard-required" name = "confirmpassword" label="Confirm Password" type="password" onChange={this.confirmPasswordHandler} error={this.state.confrimPasswordError}/>
+        </div>
+        <div>
+          <Button onClick={()=>{this.confirmSubmission()}}>Reset Password</Button>
+        </div>
+        </Box>
+      </form>
+    );
   }
 }

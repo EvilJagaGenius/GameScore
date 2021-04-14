651f767f841a96270b949a1f8fb5b764ded573b0
EvilJagaGenius <EvilJagaGenius@users.noreply.github.com>
2021-04-14 09:35:29 -0400

    Rate button updates correctly

--- game-score/src/BottomUI.jsx
+++ game-score/src/BottomUI.jsx
@@ -582,8 +582,12 @@ export default function BottomUI(props) {
                                                         })
                                                     }
                                                     fetch('/api/rateTemplate', requestOptions)
-                                                    setRatingPopup(false)
-                                                    props.update()
+                                                    .then(setRatingPopup(false))
+                                                    .then(res => res.json())
+															.then(data => {
+																props.update();
+															})
+                              .then(setAlertText("Rating submitted")).then(setShowAlert(true))
                                                 }}
                                                 
                                             />
